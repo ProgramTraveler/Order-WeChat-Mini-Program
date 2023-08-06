e3f0ea5e7791d6ead71033b3ccb4c73c8ed622f2
PrpgramTraveler <2115655391@qq.com>
2023-08-06 17:25:03 +0800

    2023-08-06

--- pro/cloud/login/index.js
+++
@@ -1,9 +0,0 @@
-const cloud = require('wx-server-sdk')
-cloud.init({
-  env:"supermarket-2pc4d" 
-
-})
-exports.main = (event) => { 
-  return { ...event,...cloud.getWXContext()}
-}
-//可同时支持返回openid和处理cloudID，获取电话号码和群ID等信息。
